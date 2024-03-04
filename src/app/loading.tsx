c9048e246f4aa0277d18380a89db92beea8ec4f6
Kemuel Germones <kemuelgermones@gmail.com>
2024-03-04 15:08:13 +0800

    Add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="px-4 py-8 mx-auto max-w-lg flex flex-col items-center gap-4">
+      <div className="text-4xl font-bold text-center">LOADING...</div>
+      <div className="text-center">
+        "I learned that if you want to make it bad enough, no matter how bad it
+        is, you can make it." -Gale Sayers
+      </div>
+    </div>
+  );
+}
